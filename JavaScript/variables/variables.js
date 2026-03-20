f07d08a3bb3e64e3ac68fe43b512d736950ba17d
morewme <morewme@outlook.com>
2026-03-19 21:23:33 -0300

    Inclusão de estudos sobre HTML

--- JavaScript/variables/variables.js
+++ JavaScript/variables/variables.js
@@ -7,3 +7,10 @@ console.log("Add x and y:", x + y);
 console.log("Subtract y from x:", x - y);
 console.log("Multiply x and y:", x * y);
 console.log("Divide x by y:", x / y);
+
+const produtos = [
+  { nome: "camisa", preco: 80,  disponivel: true  },
+  { nome: "calça",  preco: 150, disponivel: false },
+  { nome: "boné",   preco: 40,  disponivel: true  },
+  { nome: "jaqueta", preco: 300, disponivel: true },
+]
